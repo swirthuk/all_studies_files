2f36bfa09292a67f93cc3247fe79a5d7eb0e07ed
user <atantserev@bk.ru>
2026-09-12 22:29:41 +0700

    Something was deleted and added to the folder

--- КИС/zero.java
+++ КИС/zero.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+class Zero {
 
-    public static ArrayList<Integer> enterDigits() {
+    private ArrayList<Integer> lst;
+
+    public Zero() {
+        this.lst = new ArrayList<>();
+    }
+
+    public Zero(ArrayList<Integer> lst) {
+        this.lst = lst;
+    }
+
+    public Zero(Zero other) {
+        this.lst = new ArrayList<>(other.lst);
+    }
+
+    public ArrayList<Integer> getLst() {
+        return lst;
+    }
+
+    public void setLst(ArrayList<Integer> lst) {
+        this.lst = lst;
+    }
+
+    public ArrayList<Integer> enterDigits() {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -14,7 +36,7 @@ public class Main {
         return list;
     }
 
-    public static void bubbleSort(ArrayList<Integer> lst) {
+    public void bubbleSort(ArrayList<Integer> lst) {
         int n = lst.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -27,16 +49,20 @@ public class Main {
         }
     }
 
-    public static void coutArray(ArrayList<Integer> lst) {
+    public void coutArray(ArrayList<Integer> lst) {
         int n = lst.size();
         for (int i = 0; i < n; i++) {
             System.out.print(lst.get(i) + " ");
         }
     }
+}
 
+public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> lst = enterDigits();
-        bubbleSort(lst);
-        coutArray(lst);
+        Zero lab = new Zero();
+        ArrayList<Integer> lst = lab.enterDigits();
+        lab.setLst(lst);
+        lab.bubbleSort(lab.getLst());
+        lab.coutArray(lab.getLst());
     }
 }
