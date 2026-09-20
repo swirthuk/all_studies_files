51c57986b76e07a12b8940865097a550ca97908e
user <atantserev@bk.ru>
2026-09-20 22:23:06 +0700

    Java the first lab

--- Java/First.java
+++ Java/First.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class First {
     // Конструкторы
-    //геттеры и сеттеры
+    // геттеры и сеттеры
 
     public void theShortestString(int n) {
         int minLen = 10000000;
@@ -17,6 +19,24 @@ public class First {
             }
         }
         scanner.close();
-        System.out.println("Minimal string is" + minWord + "\nIts length is " + minLen);
+        System.out.println("Minimal string is " + minWord + "\nIts length is " + minLen);
+    }
+
+    public void repetitionOfWords(String text) {
+        String[] words = text.trim().split("[\\s.,?!]+");
+        Map<String, Integer> wordAndCount = new HashMap<>();
+
+        for (String word : words) {
+            if (wordAndCount.containsKey(word)) {
+                int count = wordAndCount.get(word);
+                wordAndCount.put(word, count + 1);
+            } else {
+                wordAndCount.put(word, 1);
+            }
+        }
+
+        for (String word : wordAndCount.keySet()) {
+            System.out.println(word + " - " + wordAndCount.get(word));
+        }
     }
 }
